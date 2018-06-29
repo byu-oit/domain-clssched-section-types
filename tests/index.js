5b9484f737c6f6ff877782c1e8d389aa1bb85311
lmr-99 <lyndseymrichmond@gmail.com>
2018-06-29 16:39:31 -0600

    moved swagger test to run when server starts. add example into swagger

--- tests/index.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const swagger = require('sans-server-swagger');
-
-return swagger.testSwaggerResponseExamples(path.resolve(__dirname, '../swagger.json'))
-  .then(results => console.log(results))
-  .catch(function (error) {
-    console.log(error);
-  });
