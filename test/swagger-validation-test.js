a65aca29b1000cf05007aa00a63570fa9b6bc7b6
lmr-99 <lyndseymrichmond@gmail.com>
2018-07-06 14:17:12 -0600

    add path var

--- test/swagger-validation-test.js
+++ test/swagger-validation-test.js
@@ -1,4 +1,6 @@
 const SansServerSwagger = require('sans-server-swagger')
+const path = require('path')
+
 SansServerSwagger.testSwaggerResponseExamples(path.resolve(__dirname, '../swagger.json'))
   .then(results => console.log(results))
   .catch(function (error) {
